8992d668e4add7c5f02370b11070e6af06d1b49f
Manish shrestha <MR_M4SH@proton.me>
2026-01-02 07:33:51 +0545

    Implement Socket.io initialization and connection handling

---
+++ bramanda-backend/src/config/socket.js
@@ -0,0 +1,28 @@
+let io;
+
+const initSocket = (server) => {
+  io = require("socket.io")(server, {
+    cors: {
+      origin: "*"
+    }
+  });
+
+  io.on("connection", (socket) => {
+    console.log("Socket connected:", socket.id);
+
+    socket.on("disconnect", () => {
+      console.log("Socket disconnected:", socket.id);
+    });
+  });
+
+  return io;
+};
+
+const getIO = () => {
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
+  return io;
+};
+
+module.exports = { initSocket, getIO };
